8730d2841656a466f2f544ca646162efed504b4a
Houtan Bastani <houtan@dynare.org>
2014-11-14 15:42:55 +0100

    add non-simplified NILE example (from Alessandro)

---
+++ examples/nile3eq/NILE.m
@@ -0,0 +1,58 @@
+% --------------------------------------------------------------------
+% State-spa%e format:   y(t) = C(t)z(t) + H(t)x(t)   + G(t)u(t)
+%                       x(t) = A(t)     + F(t)x(t-1) + R(t)u(t)
+%
+% y(t) (ny x 1)          ny  = # of endogenous series
+% z(t) (nz x 1)          nz  = # of exogenous series
+% x(t) (nx x 1)          nx  = # of %ontinous states
+% u(t) (nu x 1)          nu  = # of sho%ks
+% C(t) (ny x nz x ns1)   ns1 = # of states for %(t)
+% H(t) (ny x nx x ns2)   ns2 = # of states for H(t)
+% G(t) (ny x nu x ns3)   ns3 = # of states for G(t)
+% A(t) (nx x ns4)        ns4 = # of states for a(t)
+% F(t) (nx x nx x ns5)   ns5 = # of states for F(t)
+% R(t) (nx x nu x ns6)   ns6 = # of states for R(t)
+%
+% Model:
+%   y(t)  = mu(t)+c(t)
+%   c(t)  = [S(1t)*delta^(1/2)+(1-S(1t))]*Ve^(1/2)*e(t)
+%   mu(t) = mu(t-1)+S(2t)*Vepsilon^(1/2)*epsilon(t)
+%
+% State-space representation:
+%   x(t) = (mu(t),c(t))
+%   u(t) = (e(t),epsilon(t))
+%
+% Paramaeters:
+% Ve    = theta(1)
+% Vmu   = theta(2)
+% delta = theta(3)
+% ---------------------------------------------------------------------
+function [ C,H,G,A,F,R ] = NILE( ny,nz,nx,nu,ns,theta )
+% *** The following declaration are fixed ***
+C = zeros(ny,max(1,nz),ns(1));
+H = zeros(ny,nx,ns(2));
+G = zeros(ny,nu,ns(3));
+A = zeros(nx,ns(4));
+F = zeros(nx,nx,ns(5));
+R = zeros(nx,nu,ns(6));
+% *** Do not change the lines above ***
+% *** Set non-zero elements below   ***
+
+% C(t) (ny x max(1,nz) x ns1)
+
+% H(t) (ny x nx x ns2)
+H(1,1,1) = 1;
+H(1,2,1) = 1;
+
+% G(t) (ny x nu x ns3)
+
+% A(t) (nx x ns4)
+
+% F(t) (nx x nx x ns5)
+F(1,1,1) = 1;
+
+% R(t) (nx x nu x ns6)
+R(1,2,[2 4]) = sqrt(theta(2));
+R(2,1,[1 2]) = sqrt(theta(1));
+R(2,1,[3 4]) = sqrt(theta(3)*theta(1));
+
